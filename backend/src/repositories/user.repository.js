26abfd9da22326705bccc0bdd9dd9208abc0b70d
mahak <kaushikikes@gmail.com>
2026-08-05 19:38:18 +0530

    feat: implement user repository layer

---
+++ backend/src/repositories/user.repository.js
@@ -0,0 +1,18 @@
+import User from '../models/User.js'
+
+export const createUser= async ({fullname,email,password})=>{
+
+return await User.create({
+fullname,
+email,
+password  
+});
+}
+
+export const findByEmail= async(email)=>{
+return await User.findOne({email});
+}
+
+export const findById = async(id)=>{
+return await User.findById(id);
+}
