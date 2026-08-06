0c56076ba5d2c0fdfc663ed15b42d2300581bd68
mahak <kaushikikes@gmail.com>
2026-08-06 13:54:53 +0530

    feat:create login controller

--- backend/src/controllers.js/auth.controller.js
+++ backend/src/controllers.js/auth.controller.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../errors/ApiError.js";
-import { createUserService } from "../services/auth.service.js";
+import { createUserService,authUserService } from "../services/auth.service.js";
 
 export const signup = asyncHandler(async (req, res) => {
   // Step 1: Get data from client
@@ -24,4 +24,31 @@ export const signup = asyncHandler(async (req, res) => {
     message: "User registered successfully",
     data: user,
   });
+});
+
+export const logIn = asyncHandler(async (req,res)=>{
+
+   // Step 1: Get data from client
+  const{email,password} = req.body;
+
+  // Step 2: Validate input
+  if (!email?.trim() || !password?.trim()) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const {token,user} = await authUserService({email,password});
+
+   res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite:"strict",
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(200).json({
+      success:true,
+      message:"login successful",
+      data:user
+    })
 });
